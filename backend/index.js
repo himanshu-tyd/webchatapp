0a7df2c3d6eda4e74c266ada0d8fe4056f95e237
himanshu-tyd <himanshutaviyad0072@gmail.com>
2024-09-09 18:42:14 +0530

    deploy file  added.

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import authRouter from "./routes/authRoute.js";
 import messsageRouter from "./routes/messageRoute.js";
 import usersRouter from "./routes/usersRoute.js";
 import { app, server } from "./socket/socket.js";
+import path from "path"
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ const options = {
   credentials: true,
 };
 
+const __dirname=path.resolve() //TODO: To get the path of the current file
+
 //TODO:MIDDLWARES
 
 app.use(express.json()); //TODO: To parse the incoming request with JSON payloads from (req.body)
@@ -25,6 +28,12 @@ app.use(cors(options));
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messsageRouter);
 app.use("/api/users", usersRouter);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')))  //TODO: To serve static files
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname, 'frontend','dist','index.html'))  //TODO: send to file using backend
+})
 
 server.listen(8000, () => {
   connectDB();
